aed054537b8527a539fa8bcbe2fbeae885ddd8b0
minjeong <snailmin27@gmail.com>
2023-11-25 23:51:24 +0900

    Index | AboutMe | Login 일부 구현

---
+++ js/index.js
@@ -0,0 +1,17 @@
+const showHideNavMenu = () => {
+    const nav = document.querySelector('#main-nav');
+    nav.classList.toggle('nav-hide-position');
+
+    console.log(nav.classList);
+};
+const hideNavMenu = () => {
+    const nav = document.querySelector('#main-nav');
+    nav.classList.toggle('nav-hide-position');
+
+    console.log(nav.classList);
+};
+
+// bgc
+// document.querySelector("#main-section-intro div:first-child h1").addEventListener('click', () => {
+//     console.log('dddd');
+// });
